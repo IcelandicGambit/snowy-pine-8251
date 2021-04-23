3fe1d0812ffd86c0bcbe8975b39c4adf603e41b8
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-23 09:54:55 -0700

    start user story 3 tests

--- spec/features/airlines_show_spec.rb
+++ spec/features/airlines_show_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Flights Index Page" do
+RSpec.describe "Flights Show Page" do
   before :each do
     @airline = Airline.create!(name: "Larsh Airlines")
+    @airline2 = Airline.create!(name: "Plamb Airlines")
     @flight_1 = Flight.create!(number: "1999", date: "07/16/21", departure_city: "Seattle", arrival_city: "Denver", airline: @airline)    
     @flight_2 = Flight.create!(number: "5454", date: "07/13/21", departure_city: "New York", arrival_city: "Los Angeles", airline: @airline)
     @passenger1 = Passenger.create!(name: "Stephen Tsimicalis", age: 28)
     @passenger2 = Passenger.create!(name: "Larsh Homestead", age: 33)
     @passenger3 = Passenger.create!(name: "Pierce Lamb", age: 20)
     @passenger4 = Passenger.create!(name: "David Lamb", age: 34)
-    @passenger5 = Passenger.create!(name: "Kohl Miller", age: 65)
+    @passenger5 = Passenger.create!(name: "Kohl Miller", age: 8)
 
     FlightPassenger.create(flight_id: @flight_1.id, passenger_id: @passenger1.id)
     FlightPassenger.create(flight_id: @flight_1.id, passenger_id: @passenger2.id)
@@ -20,28 +21,23 @@ RSpec.describe "Flights Index Page" do
     FlightPassenger.create(flight_id: @flight_2.id, passenger_id: @passenger5.id)
   end
 
-  it "shows a list of flights with a list of passengers for each flight" do
-  visit "/flights"
+  it "lists passengers that have flights on that airline" do
+    visit "/airline/#{@airline.id}"
 
-
-    expect(page).to have_content(@airline.name)
-    expect(page).to have_content(@flight_1.number)
-    expect(page).to have_content(@passenger1.name)
-    expect(page).to have_content(@passenger2.name)
-    expect(page).to have_content(@passenger3.name)
-    expect(page).to have_content(@passenger4.name)
-  
-    expect(page).to have_content(@flight_2.number)
-    expect(page).to have_content(@passenger4.name)
-    expect(page).to have_content(@passenger5.name)
+    expect(page).to have_content(passenger1.name)
+    expect(page).to have_content(passenger2.name)
+    expect(page).to have_content(passenger3.name)
+    expect(page).to have_content(passenger4.name)
+    expect(page).to have_content(passenger5.name)
   end
 
-  it "delete button removes passenger" do
-  visit "/flights"
+  it "shows that list has no duplicate names of passengers" do
+    visit "/airline/#{@airline.id}"
 
-    within("#passenger-#{@passenger_.id}") do
-      expect(page).to have_content(@passenger_1.name)
-      click_button('Remove Passenger')
-    end
+    expect(page).to have_content(passenger4.name)
+  end
+
+  it "only shows passengers that are 18 or older" do
+    visit "/airline/#{@airline.id}"
   end
 end
