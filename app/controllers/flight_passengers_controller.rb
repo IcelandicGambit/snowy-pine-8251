77145bc5c2e369b23056b605b51fd4be6c924839
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-23 09:01:11 -0700

    change controller destroy method

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,2 +1,8 @@
 class FlightPassengersController < ApplicationController
+  def destroy
+    # passenger = Passenger.find(params[:passenger_id])
+    # passenger.flights.destroy(params[:flight_id])
+    # redirect_to '/flights'
+  FlightPassengers.where(passenger_id: params[:passenger], flight_id: params[:flight]).destroy_all
+  end
 end
