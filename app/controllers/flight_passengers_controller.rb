2e68cee529820ca5f743f86ec6af0f49a05ee2bc
Andrew Smith <andrewpatrick138@gmail.com>
2021-04-23 17:43:41 -0700

    refactor 1

--- app/controllers/flight_passengers_controller.rb
+++ app/controllers/flight_passengers_controller.rb
@@ -1,8 +1,6 @@
 class FlightPassengersController < ApplicationController
   def destroy
-    # passenger = Passenger.find(params[:passenger_id])
-    # passenger.flights.destroy(params[:flight_id])
-    # redirect_to '/flights'
     FlightPassenger.find_by(passenger_id: params[:passenger_id], flight_id: params[:id]).destroy
+    redirect_to "/flights"
   end
 end
